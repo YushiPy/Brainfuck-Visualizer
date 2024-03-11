5b671461ed2a99b990c58083f5f638721c4e8b8f
Yushi <gabriel_ushijima@hotmail.com>
2024-03-10 21:17:04 -0300

    Defining a private parser function. This will turn a string into a list of functions to be called

--- interpreter.py
+++ interpreter.py
@@ -5,7 +5,7 @@ from tape import Tape
 
 type Routine = list[tuple[Callable[[], None]] | 
                     tuple[Callable[[int], None], int] | 
-                    tuple[Callable[[int], None], int, int]]
+                    tuple[Callable[[int, int], None], int, int]]
 
 class Interpreter(Tape):
     
@@ -27,6 +27,10 @@ class Interpreter(Tape):
     
     
     def find_match(self, index: int) -> int:
+        """
+        returns the index of the matching closing bracket
+        `index` shoud be the index of the opening bracket
+        """
         
         count = 1
         
@@ -37,6 +41,41 @@ class Interpreter(Tape):
         return index
     
     
+    def __parse(self, start: int | None = None, end: int | None = None) -> Routine:
+        
+        if start is None: start = 0
+        if end is None: end = len(self.code)
+        
+        index = start
+        routine: Routine = []
+        
+        while index < end:
+            
+            char = self.code[index]
+            
+            if char == '>': routine.append((self.move_by, 1))
+            if char == '<': routine.append((self.move_by, -1))
+            if char == '+': routine.append((self.increase, 1))
+            if char == '-': routine.append((self.increase, -1))
+            if char == ',': routine.append((self.read_byte,))
+            if char == '.': routine.append((self.output_byte,))
+            
+            if char != '[': 
+                index += 1
+                continue
+            
+            inside_end = self.find_match(index)
+            inside_routine = self.__parse(index + 1, inside_end - 1) # +1 and -1 to remove brackets
+
+            def inside() -> None:
+                
+                for a, *b in inside_routine:
+                    a(*b)
+            
+            routine.append((inside,))
+        
+        return routine
+
     
     @overload
     def run(self, _input: list[int | str] | str) -> list[int]: ...
