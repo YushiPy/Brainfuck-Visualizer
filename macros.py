8d1171b9c8d8a8bdf2415d7fa0e6e72b73671d53
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-07-13 00:18:24 -0300

    Adding xor macro

--- macros.py
+++ macros.py
@@ -80,6 +80,31 @@ def _and(on_stack: str) -> str:
 	return ">>>>[-]>>[-]<<<<<<[->>>>+>>+<<<<<<]>>>>>>[-<<<<<<+>>>>>>]<<<<<<>>>>>>[-]>>[-]<<<<<<[->>>>+>>+<<<<<<]>>>>>>[-<<<<<<+>>>>>>]<<<<<<>>[[-]>>>>+<<<<]>>[[-]>>+<<]<<+>>>>--[[-]<<<<[-]>>>>]<<<<<<<<"
 
 
+def _xor(on_stack: str) -> str:
+	"""
+	Returns a code snippet for computing `a ^ b`.
+	
+	Parameter
+	--------
+	- `on_stack == 0`: Returns a comparison on stack level.
+	- `on_stack != 0`: Returns a comparison on information level.
+
+	Results
+	-------
+	Initial = `[a, x, b, x, x, x]`.
+
+	Final = {
+	- `on_stack == 0`: `[a, x, b, x, a ^ b, x, 0, x, 0]`.
+	- `on_stack != 0`: `[a, a ^ b, b, 0, x, 0]`.
+	}
+	"""
+
+	if on_stack == "0":
+		return ">>>>[-]>>[-]<<<<<<[->>>>+>>+<<<<<<]>>>>>>[-<<<<<<+>>>>>>]<<<<<<>>>>>>[-]>>[-]<<<<<<[->>>>+>>+<<<<<<]>>>>>>[-<<<<<<+>>>>>>]<<<<<<>>[[-]>>>>+<<<<]>>[[-]>>+<<]>>[--[[-]<<<<+>>>>]]<<<<<<<<"
+
+	return ">[-]>>[-]<<<[->+>>+<<<]>>>[-<<<+>>>]<<<>>>[-]>>[-]<<<[->+>>+<<<]>>>[-<<<+>>>]<<<>[[-]>>+<<]<<[[-]>>>>+<<<<]>>>>[--[[-]<<<<+>>>>]]<<<<<"
+
+
 def _eq(on_stack: str) -> str:
 	"""
 	Returns a code for computing `a == b`.
@@ -112,5 +137,6 @@ MACROS: dict[str, Callable[[str], str]] = {
 	"copy": _copy,
 	"or": _or,
 	"and": _and,
+	"xor": _xor,
 	"eq": _eq,
 }
