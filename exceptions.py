d01c9f95b0fe5b98f812c657b8e32ac3c50e242a
Gabriel Freire Ushijima <gabriel_ushijima@hotmail.com>
2024-07-12 23:04:35 -0300

    Adding expections related to macros

--- exceptions.py
+++ exceptions.py
@@ -1,20 +1,33 @@
 
 class PointerOutOfBound(Exception):
 
-    @staticmethod
-    def negative_pointer(index: int) -> "PointerOutOfBound":
-        return PointerOutOfBound(f"Tried to move to negative index; pointer = {index};")
+	@staticmethod
+	def negative_pointer(index: int) -> "PointerOutOfBound":
+		return PointerOutOfBound(f"Tried to move to negative index; pointer = {index}.")
 
-    @staticmethod
-    def too_big_pointer(index: int, size: int) -> "PointerOutOfBound":
-        return PointerOutOfBound(f"Tried to move beyond last byte; pointer = {index:_} >= size = {size:_}")
+	@staticmethod
+	def too_big_pointer(index: int, size: int) -> "PointerOutOfBound":
+		return PointerOutOfBound(f"Tried to move beyond last byte; pointer = {index:_} >= size = {size:_}.")
 
 class MismatchedBrackets(Exception):
-    
-    @staticmethod
-    def mismatched(code: str, index: int, depth: int) -> "MismatchedBrackets":
+	
+	@staticmethod
+	def mismatched(code: str, index: int, depth: int) -> "MismatchedBrackets":
 
-        string: str = f"Couldn't find match for bracket at {index=}; " 
-        string += f"{depth} closing bracket{'s' * (depth > 1)} missing in {code=};"
+		string: str = f"Couldn't find match for bracket at {index=}; " 
+		string += f"{depth} closing bracket{'s' * (depth > 1)} missing in {code=}."
 
-        return MismatchedBrackets(string)
+		return MismatchedBrackets(string)
+
+class InvalidMacro(Exception):
+
+	@staticmethod
+	def invalid_name(name: str) -> "InvalidMacro":
+		return InvalidMacro(f"Macro \"{name}\" does not exist.")
+
+	@staticmethod
+	def mismatched_parenthesis(macro: str, index: int) -> "InvalidMacro":
+
+		string: str = f"Couldn't match parenthesis at index={index} for macro=\"{macro}\"."
+
+		return InvalidMacro(string)
